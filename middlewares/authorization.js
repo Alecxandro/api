7ebbadcf65a7c4cf33c0cbe38eca057a688ef69d
Alex <alecxandroch@gmail.com>
2024-09-24 15:53:00 -0300

    chore: minor changes

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,23 +1,20 @@
 import modelPermissions from './modelPermissions.js'
 import bcrypt from 'bcryptjs'
 
-export const authorizationAction = async (req, res, model, action = 'read', modelName = 'users') => {
+const authorizationAction = async (req, res, model, action = 'read', modelName = 'users') => {
     const { role } = req.user
     const { id } = req.params
 
     const permissions = modelPermissions[modelName]
-    console.log(`Role: ${role}, Action: ${action}, ModelName: ${modelName}`)
 
     if (!permissions) {
-        console.log('Invalid model:', modelName)
         res.status(400).json({ message: `Invalid model: ${modelName}` })
         return null
     }
 
     const actionPermissions = permissions.canCRUD
-    console.log('Action Permissions:', actionPermissions)
+
     if (!actionPermissions) {
-        console.log('Invalid action:', action)
         res.status(403).json({ message: `Invalid action: ${action}` })
         return null
     }
@@ -25,7 +22,6 @@ export const authorizationAction = async (req, res, model, action = 'read', mode
     try {
         if (role === 'instructor' || role === 'student') {
             if (id !== String(req.user._id)) {
-                console.log('Not authorized to perform this action')
                 res.status(403).json({
                     message: 'You are not authorized to perform this action',
                 })
@@ -36,7 +32,6 @@ export const authorizationAction = async (req, res, model, action = 'read', mode
         if (id === String(req.user._id)) {
             const user = await model.findById(req.user._id)
             if (!user) {
-                console.log('User not found')
                 res.status(404).json({ message: 'User not found' })
                 return null
             }
@@ -56,17 +51,14 @@ export const authorizationAction = async (req, res, model, action = 'read', mode
 
         const targetDocument = await model.findById(id)
         if (!targetDocument) {
-            console.log('Target document not found')
             res.status(404).json({ message: 'Target document not found' })
             return null
         }
 
         const targetRole = modelName === 'users' ? targetDocument.role : modelName
         const allowedRoles = actionPermissions[role]
-        console.log('Allowed Roles:', allowedRoles) // Log allowed roles
 
         if (!allowedRoles.includes(targetRole)) {
-            console.log('Not authorized to perform this action on target role')
             res.status(403).json({
                 message: 'You are not authorized to perform this action',
             })
@@ -88,21 +80,17 @@ export const authorizationAction = async (req, res, model, action = 'read', mode
                 document = await model.findByIdAndDelete(id)
                 break
             default:
-                console.log('Invalid action')
                 res.status(400).json({ message: 'Invalid action' })
                 return null
         }
 
         if (!document && action !== 'register') {
-            console.log('Document not found')
             res.status(404).json({ message: 'Document not found' })
             return null
         }
 
-        console.log('Document successfully processed:', document) // Log the created or found document
         return document
     } catch (error) {
-        console.log('Error occurred:', error.message)
         res.status(500).json({
             message: `An error has occurred. Details: ${error.message}`,
         })
@@ -110,10 +98,11 @@ export const authorizationAction = async (req, res, model, action = 'read', mode
     }
 }
 
-export const handleResponse = (res, statusCode, message, data = null) => {
+const handleResponse = (res, statusCode, message, data = null) => {
     if (res.headersSent) {
-        console.warn('Attempted to send headers after they were already sent.')
         return
     }
     res.status(statusCode).json(data ? { message, data } : { message })
 }
+
+export { handleResponse, authorizationAction }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,11 +3,11 @@ import * as userController from '../controllers/users/user.controller.js'
 import { authenticateJWT } from '../middlewares/authentication.js';
 
 const router = express.Router();
+router.get('/allusers', authenticateJWT, userController.getAllUsers)
+router.get('/:id', authenticateJWT, userController.getUser)
 
 router.post('/register', authenticateJWT, userController.registerUser)
-router.delete('/delete/:id', authenticateJWT, userController.deleteUser)
-router.get('/:id([0-9]+)', authenticateJWT, userController.getUser)
-router.get('/allusers', authenticateJWT, userController.getAllUsers)
 router.post('/login', userController.loginUser)
 
+router.delete('/delete/:id', authenticateJWT, userController.deleteUser)
 export default router;
